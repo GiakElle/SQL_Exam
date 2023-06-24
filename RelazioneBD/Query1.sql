dfb48d5a43e01e2af84f4bed42e86b2cddb7ede1
GiakElle <105581206+GiakElle@users.noreply.github.com>
2023-06-24 15:37:05 +0200

    Delete RelazioneBD directory

--- RelazioneBD/Query1.sql
+++
@@ -1,3 +0,0 @@
-select Id_alunno
-from Storicoiscrizione as s, Classe, Scuola
-Where AS=2020 and S.id_classe=Classe.ID and Classe.id_scuola=Scuola.ID and Scuola.tipo='SS'
--- RelazioneBD/Query2.sql
+++
@@ -1,3 +0,0 @@
-select id_ins
-from Insegna as x, Insegna as y, Classe as c1, Classe as c2 
-where x.id_ins=y.id_ins and x.id_classe= c1.ID and y.id_classe= c2.ID and c1.id_scuola<>c2.id_scuola
